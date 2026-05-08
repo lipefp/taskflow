3a78f983d06fa554742e635bb0af930e9268cc3c
lipefp <dinizlipe27@gmail.com>
2026-05-08 17:52:18 -0300

    [feat]: views do CRUD de tarefas

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.exceptions import PermissionDenied
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+
+class TaskListCreateView(generics.ListCreateAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
+
+    def get_object(self):
+        objeto = super().get_object()
+        if objeto.user != self.request.user:
+            raise PermissionDenied('Você não tem permissão para realizar esta ação.')
+        return objeto
